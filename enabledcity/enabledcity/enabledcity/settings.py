4696ad876a9bc6ff53918e220876cfa18fdf94dd
Svetlana <svetlanamargetova@gmail.com>
2014-05-27 00:41:39 +0200

    'Database_settings'

--- enabledcity/enabledcity/enabledcity/settings.py
+++ enabledcity/enabledcity/enabledcity/settings.py
@@ -57,10 +57,16 @@ WSGI_APPLICATION = 'enabledcity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'geolocator.sqlite3',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
